7b9d52e7e1616aaef1a7d21dee87c6df72c38b29
nxoo <nxoo.dev@gmail.com>
2022-08-22 13:32:07 +0300

    added comments

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ const points = [1, 3, 3, 2, 1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10];
 const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
     'p','q','r','s','t','u','v','w', 'x','y','z'];
 
-//log either player one win or player two win depending on 
-//who scored the highest
+// calculates score of word in the points scale
 function wordScore(word) {
     let total = 0;
     for (let x=0; x<word.length; x++) {
         // position of each letter in the alphabet
         let xAlphabetPosition = alphabet.indexOf(word[x])
-        // add value of letter in position scale to total value of word
+        // add points value of letter to total word score
         total += points[xAlphabetPosition]
     }
     return total
 }
+
 function calculateWinner(playerOneWord, playerTwoWord){
     let playerOne = wordScore(playerOneWord.toLowerCase())
     let playerTwo = wordScore(playerTwoWord.toLowerCase())
-
-    // player one wins or player two wins
     let scoreText = `${playerOneWord} ${playerOne} - ${playerTwoWord} ${playerTwo}`
+
     if (playerOne === playerTwo) {
         console.log(`Draw: ${scoreText}`)
     } else if (playerOne > playerTwo) {
