b14af02acadf8630461d620ccba44ce3bc7c1fa9
njmwasmoringa <julius.mwangi@moringaschool.com>
2022-08-22 09:45:17 +0300

    CalculateWinner Function added

---
+++ index.js
@@ -0,0 +1,15 @@
+
+// This points represent the points for each letter of alphabet
+const points = [1, 3, 3, 2, 1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10];
+
+//log either player one win or player two win depending on 
+//who scored the highest
+function calculateWinner(playerOneWord, playerTwoWord){
+
+    // write code here
+
+    console.log();
+    // player one wins or player two wins
+}
+
+calculateWinner( 'COMPUTER', 'HELLOWORLD' );
