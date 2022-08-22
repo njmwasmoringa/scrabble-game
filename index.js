95bd4b44d2f604caf60cc3d80e2848c0df3dd37c
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-08-22 12:13:00 +0300

    completed code challenge

--- index.js
+++ index.js
@@ -2,14 +2,33 @@
 // This points represent the points for each letter of alphabet
 const points = [1, 3, 3, 2, 1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10];
 
-//log either player one win or player two win depending on 
-//who scored the highest
-function calculateWinner(playerOneWord, playerTwoWord){
+//The alphabets as one string which is split to single characters
+let alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
 
-    // write code here
-
-    console.log();
-    // player one wins or player two wins
+function fetch(alp) {
+    var sum = 0;
+    for(const it of alp){
+        var index = alphabet.indexOf(it);
+        sum+=points[index];
+    }
+    return sum;
 }
 
-calculateWinner( 'COMPUTER', 'HELLOWORLD' );
+
+function calculateWinner(playerOneWord , playerTwoWord){
+        let sum1 = fetch(playerOneWord);
+        let sum2 = fetch(playerTwoWord);
+
+        if(sum1>sum2){
+            console.log(playerOneWord+" "+sum1);
+        }
+        else if(sum2>sum1){
+            console.log(playerTwoWord+" "+sum2);
+        }
+        else{
+            console.log("draw")
+        }
+console.log("winner");
+}
+
+calculateWinner("computer" , "helloworld")
